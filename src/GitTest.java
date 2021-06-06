f0d27697076f3a4a9bd52fc759cb8ed97516286a
shicaiyuana <36296882+shicaiyuana@users.noreply.github.com>
2021-06-06 14:52:53 +0800

    pull commit

--- src/GitTest.java
+++ src/GitTest.java
@@ -2,5 +2,6 @@ public class GitTest {
     public static void main(String[] args){
         System.out.println("hello git1" );
         System.out.println("push test" );
+        System.out.println("pull test" );
     }
 }
